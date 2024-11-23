8415d36de69375d00a66f2da3c6746c3c11ed960
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-11-24 00:33:11 +0200

    Add error handling to register form

--- frontend/src/app/types/register.ts
+++ frontend/src/app/types/register.ts
@@ -1,6 +1,7 @@
 export interface RegisterResponse {
     message: string;
     user: {
+        avatar: string;
         username: string;
         email: string;
     };
--- frontend/src/app/user/register/register.component.ts
+++ frontend/src/app/user/register/register.component.ts
@@ -3,33 +3,38 @@ import { AuthService } from '../auth.service';
 import { Router, RouterLink } from '@angular/router';
 import { RegisterResponse } from '../../types/register';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatchPasswordsDirective } from '../../directives/match-passwords.directive';
+import { EmailValidatorDirective } from '../../directives/validate-email.directive';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [RouterLink, FormsModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    standalone: true,
+    imports: [RouterLink, FormsModule, CommonModule, MatchPasswordsDirective, EmailValidatorDirective],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    avatar = '';
-    username = '';
-    email = '';
-    password = '';
-    rePassword = '';
-  
-    constructor(private auth: AuthService, private router: Router) {}
-  
+    user = {
+        avatar: '',
+        username: '',
+        email: '',
+        password: '',
+        rePassword: ''
+    }
+
+    constructor(private auth: AuthService, private router: Router) { }
+
     register() {
-      this.auth.register({ avatar: this.avatar, username: this.username, email: this.email, password: this.password }).subscribe(
-        (res: RegisterResponse) => {
-            console.log(res.message);
-            alert('Registration successful!');
-            this.router.navigate(['/login']);
-          },
-          (err) => {
-            alert('Registration failed. Try again.');
-          }
-      );
+        this.auth.register({ avatar: this.user.avatar, username: this.user.username, email: this.user.email, password: this.user.password }).subscribe(
+            (res: RegisterResponse) => {
+                console.log(res.message);
+                alert('Registration successful!');
+                this.router.navigate(['/login']);
+            },
+            (err) => {
+                alert('Registration failed. Try again.');
+            }
+        );
     }
 }
