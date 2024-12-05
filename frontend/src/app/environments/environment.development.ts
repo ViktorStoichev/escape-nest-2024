77acebdeecde6b23939fb799571f2733f1765ee0
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-12-05 18:09:05 +0200

    Attempt to create responsive css

--- frontend/src/app/environments/environment.development.ts
+++ frontend/src/app/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     authUrl: 'https://escape-nest-2024-backend.onrender.com/api/auth',
     postsUrl: 'https://escape-nest-2024-backend.onrender.com/api/posts'
+    // Change to http://localhost:3000/api/posts or auth if you want to run the app locally
 }
